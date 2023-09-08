0624ed7d6f1654f16c416b5c63eb5cc133dd1811
Bituc <Bituc@ALEXANDR>
2023-09-08 23:02:29 +0200

    Добавьте файлы проекта.

---
+++ Домашнее заданее 2/Program.cs
@@ -0,0 +1,37 @@
+﻿//Дано четырех значное число. Найти: 
+//а) сумму его цифр;
+//б) произведение его цифр.
+//a
+try
+//{
+//    Console.Write("введите четырехзначное число:");
+//    int n = int.Parse(Console.ReadLine());
+//    int a = n % 10;
+//    int b = n / 10 % 10;
+//    int c = n / 100;
+//    int d = n / 100;
+//    Console.Write(a);
+//    Console.Write(b);
+//    Console.Write(d);
+//    Console.Write(d);
+//    Console.WriteLine($"{a + b + c + d}");
+//}
+//catch
+//{
+//    Console.Write("Введите корекное число!");
+//}
+//b
+//try
+//{
+//    Console.Write("ВВедите черырехзначное число:");
+//    int n = int.Parse(Console.ReadLine());
+//    int a = n % 10;
+//    int b = n / 10 % 10;
+//    int c = n / 100;
+//    int d = n / 100;
+//    Console.WriteLine($"{a * b * c * d}");
+//}
+//catch 
+//{
+//    Console.Write("Введите коректное число!");
+//}
---
+++ Домашнее задание/Program.cs
@@ -0,0 +1,9 @@
+﻿//Домашнее задание вариант 8
+Console.WriteLine("Введите y");
+double y = double.Parse(Console.ReadLine());
+Console.WriteLine($"T={Math.Sqrt(6*(y*y)-0.1*y+4)}");
+
+
+
+
+
