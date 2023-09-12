1646b9423844e24bdf8fa487a54cfceaf9c83f66
Alexandro39 <144172367+Alexandro39@users.noreply.github.com>
2023-09-12 21:48:33 +0200

    дз

--- Домашнее заданее 2/Program.cs
+++ Домашнее заданее 2/Program.cs
@@ -2,36 +2,36 @@
 //а) сумму его цифр;
 //б) произведение его цифр.
 //a
-//try
-//{
-//    Console.Write("введите четырехзначное число:");
-//int n = int.Parse(Console.ReadLine());
-//int a = n % 10;
-//int b = n / 10 % 10;
-//int c = n / 100;
-//int d = n / 1000;
-//Console.WriteLine(a);
-//Console.WriteLine(b);
-//Console.WriteLine(d);
-//Console.WriteLine(d);
-//Console.WriteLine($"{a + b + c + d}");
-//}
-//catch
-//{
-//    Console.Write("Введите корекное число!");
-//}
+try
+{
+    Console.Write("введите четырехзначное число:");
+int n = int.Parse(Console.ReadLine());
+int a = n % 10;
+int b = n / 10 % 10;
+int c = n / 100;
+int d = n / 1000;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(d);
+Console.WriteLine(d);
+Console.WriteLine($"{a + b + c + d}");
+}
+catch
+{
+    Console.Write("Введите корекное число!");
+}
 //b
-//try
-//{
-//    Console.Write("ВВедите черырехзначное число:");
-//int n = int.Parse(Console.ReadLine());
-//int a = n % 10;
-//int b = n / 10 % 10;
-//int c = n / 100;
-//int d = n / 1000;
-//Console.WriteLine($"{a * b * c * d}");
-//}
-//catch 
-//{
-//    Console.Write("Введите коректное число!");
-//}
+try
+{
+    Console.Write("ВВедите черырехзначное число:");
+int n = int.Parse(Console.ReadLine());
+int a = n % 10;
+int b = n / 10 % 10;
+int c = n / 100;
+int d = n / 1000;
+Console.WriteLine($"{a * b * c * d}");
+}
+catch 
+{
+    Console.Write("Введите коректное число!");
+}
