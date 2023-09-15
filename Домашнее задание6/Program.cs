7401318b05f895dd202351e7cdd6cefb91ee8954
Alexandro39 <144172367+Alexandro39@users.noreply.github.com>
2023-09-15 23:05:37 +0200

    дз

---
+++ Домашнее задание6/Program.cs
@@ -0,0 +1,11 @@
+﻿try
+{
+    const double PI = 3.14;
+    Console.Write("Введите радиус окружности:");
+    double a = double.Parse(Console.ReadLine());
+    Console.WriteLine($"S = {PI * a}");
+}
+catch 
+{
+    Console.WriteLine("Введите коректное значение!");
+}
