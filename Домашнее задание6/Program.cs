1854e47af97820f2e50fea6fc2b36c5791cc364e
Alexandro39 <144172367+Alexandro39@users.noreply.github.com>
2023-09-22 23:46:19 +0200

    lp

--- Домашнее задание6/Program.cs
+++ Домашнее задание6/Program.cs
@@ -5,7 +5,34 @@
     double a = double.Parse(Console.ReadLine());
     Console.WriteLine($"S = {PI * a}");
 }
-catch 
+catch
 {
     Console.WriteLine("Введите коректное значение!");
 }
+
+
+
+
+try
+{
+    Console.WriteLine("Введите ваше имя" +
+        "\n Введите ваш пол:" +
+        "\n Введите ваш год рождения:" +
+        "\n Введите ваш рост:" +
+        "\n Сколько вам полных лет?:");
+    char name = char.Parse(Console.ReadLine());
+    char floor = char.Parse(Console.ReadLine());
+    int year = int.Parse(Console.ReadLine());
+    int height = int.Parse(Console.ReadLine());
+    int fullYears = int.Parse(Console.ReadLine());
+    Console.WriteLine($"{name}" +
+        $"\n {floor}" +
+        $"\n {year}" +
+        $"\n {height}" +
+        $"\n {fullYears}");
+}
+catch 
+{
+    Console.WriteLine("Введите коректное значение:");
+}
+
